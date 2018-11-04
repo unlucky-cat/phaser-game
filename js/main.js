8e680cbe37d8d3da10c3fdca460c2fbe3122278b
Andrey Morozov <andrey@morozov.lv>
2018-11-04 20:28:49 +0200

    simple grass background implemented

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 256,
+    backgroundColor: 'rgb(255, 255, 255)',
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    //this.load.setBaseURL('http://labs.phaser.io');
+
+    this.load.image('background', 'img/background.png');
+}
+
+function create ()
+{
+    this.add.image(128, 128, 'background');
+}
