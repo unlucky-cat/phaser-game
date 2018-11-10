4bd82c2c8c6abb1c9adce4e07864d724cf4b1801
Andrey Morozov <andrey@morozov.lv>
2018-11-10 17:58:14 +0200

    tileset and tilemap added

--- js/main.js
+++ js/main.js
@@ -5,26 +5,37 @@ var config = {
     backgroundColor: 'rgb(255, 255, 255)',
     pixelArt: true,
     parent: '09ji2d0ijw',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
+var map;
+var layer;
 
 function preload ()
 {
-    this.load.image('background', 'img/background.png');
+    game.load.tilemap('default', 'tiled/default_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tileset', 'img/grass_tileset_16x16.png');
+
+    //this.load.image('background', 'img/background.png');
 }
 
 function create ()
 {
-    this.add.image(128, 128, 'background');
+    map = game.add.tilemap('default');
+    map.addTilesetImage('grass', 'tileset');
+
+    layer = map.createLayer('World1');
+    layer.resizeWorld();
+
+    //this.add.image(128, 128, 'background');
+}
+
+function update ()
+{
+
 }
