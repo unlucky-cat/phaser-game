28940210fab16955a478ad88d67066a85b08c4fb
Andrey Morozov <andrey@morozov.lv>
2018-11-11 11:57:17 +0200

    switched back to JSON tilemaps but fixed the "cannot read property 2 of undefined" error

--- js/main.js
+++ js/main.js
@@ -16,14 +16,34 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-
-	this.load.image('tileset', 'img/grass_tileset_16x16.png');
-    this.load.tilemapCSV('light', 'tiled/default_map_light.csv');
-    this.load.tilemapCSV('normal', 'tiled/default_map_normal.csv');
+    preloadJSON.call(this);
 }
 
 function create ()
 {
+
+    createJSON.call(this);
+}
+
+function update ()
+{
+
+}
+
+var preloadCSV = function() {
+    this.load.image('tileset', 'img/grass_tileset_16x16.png');
+    
+    this.load.tilemapCSV('light', 'tiled/default_map_light.csv');
+    this.load.tilemapCSV('normal', 'tiled/default_map_normal.csv');
+}
+
+var preloadJSON = function() {
+    this.load.image('tileset', 'img/grass_tileset_16x16.png');
+    
+    this.load.tilemapTiledJSON('map', 'tiled/default_map_corrected.json');
+}
+
+var createCSV = function() {
 
     var map = this.make.tilemap({ key: 'light', tileWidth: 16, tileHeight: 16 });
     var map2 = this.make.tilemap({ key: 'normal', tileWidth: 16, tileHeight: 16 });
@@ -33,10 +53,14 @@ function create ()
 
     var layer = map.createDynamicLayer(0, tileset, 0, 0);
     var layer2 = map2.createDynamicLayer(0, tileset2, 0, 0);
-
 }
 
-function update ()
-{
+var createJSON = function() {
 
+    var map = this.make.tilemap({ key: 'map' });
+
+    var tileset = map.addTilesetImage('grass', 'tileset');
+
+    var layer1 = map.createDynamicLayer('light', tileset);
+    var layer2 = map.createDynamicLayer('normal', tileset);
 }
